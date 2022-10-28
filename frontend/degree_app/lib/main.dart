af766d5239a856c341aae2270136ebbeafb559de
serezhia <dev@serezhia.ru>
2022-10-28 22:06:56 +0300

    init frontend (flutter project)

---
+++ frontend/degree_app/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Test());
+}
+
+class Test extends StatelessWidget {
+  const Test({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
